2409e9ed3d89d97d053dbb18907e7e6dd18d697e
jinah-byun <wlsdk120@gmail.com>
2023-02-24 14:01:26 +0900

    solved:'pg/사칙연산/E/5

--- Baekjoon/solution.js
+++ Baekjoon/solution.js
@@ -1,5 +1,14 @@
 // 프로그래머스 level0 test 용
-// 두수의 합
-function solution(num1, num2) {
-  return num1 + num2;
-}
+// 두수의 합, 차, 곱, 나누기 (화살표 함수이용)
+
+//합
+const solution1 = (num1, num2) => num1 + num2;
+
+//차
+const solution2 = (num1, num2) => num1 - num2;
+
+//곱
+const solutio3 = (num1, num2) => num1 * num2;
+
+//나누기
+const solution4 = (num1, num2) => Math.floor(num1 / num2);
