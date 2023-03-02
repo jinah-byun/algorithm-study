37680aa98071389f7f3b51d94ee81fbeedd8bb05
jinah-byun <wlsdk120@gmail.com>
2023-03-02 23:14:54 +0900

    solved: pg/피자나눠먹기/N/5

---
+++ index.js
@@ -0,0 +1,16 @@
+//test
+
+// const fs = require("fs");
+// const inputDate = fs
+//   .readFileSync("example.txt")
+//   .toString()
+//   .split("")
+//   .map((value) => +value);
+
+// if (a > b) {
+//   console.log(">");
+// } else if (a < b) {
+//   console.log("<");
+// } else {
+//   console.log("==");
+// }
