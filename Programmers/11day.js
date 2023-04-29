0d00d459b6bb116276bcc988272f2295d984d63c
jinah-byun <wlsdk120@gmail.com>
2023-04-29 22:40:54 +0900

    refactor: pg/합성수 찾기/H/11day

--- Programmers/11day.js
+++ Programmers/11day.js
@@ -34,6 +34,30 @@ function solution(n) {
       answer += 1;
     }
   }
+  return answer;
+}
 
+//2-1.refactor 합성수 찾기
+
+const getISCount = (i) => {
+  let isCount = false;
+  for (let num = 2; num < i - 1; num++) {
+    if (i % num === 0) {
+      //합성수
+      isCount = true;
+    }
+  }
+  return isCount;
+};
+function solution(n) {
+  let answer = 0;
+  for (let i = n; i >= 1; i--) {
+    // i가 합성수인지 비교해야하는 숫자
+    const isCount = getISCount(i);
+    // isCount => 합성수 여부를 확인할 수 있음.
+    if (isCount) {
+      answer += 1;
+    }
+  }
   return answer;
 }
