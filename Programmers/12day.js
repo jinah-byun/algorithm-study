a775b2e443667680f8b319e3de72a50cb35751fc
jinah-byun <wlsdk120@gmail.com>
2023-05-01 23:23:13 +0900

    메모

---
+++ Programmers/12day.js
@@ -0,0 +1,33 @@
+<table>
+  <caption>학생별 과목 평균점수</caption>
+
+  <thead>
+    <tr>
+      <th scope="col">이름</th>
+      <th scope="col">과목</th>
+      <th scope="col">평균점수</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>김철수</td>
+      <td>영어</td>
+      <td>80점</td>
+    </tr>
+    <tr>
+      <td>김세미</td>
+      <td>수학</td>
+      <td>60점</td>
+    </tr>
+    <tr>
+      <td>김티스토리</td>
+      <td>체육</td>
+      <td>40점</td>
+    </tr>
+    <tr>
+      <td>철수세미</td>
+      <td>음악</td>
+      <td>100점</td>
+    </tr>
+  </tbody>
+</table>;
