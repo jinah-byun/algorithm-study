a883795cb3454fc50987e06db61d48b94d1973ee
jinah-byun <wlsdk120@gmail.com>
2023-05-02 23:32:47 +0900

    sovled:pg/모음제N/q

--- Programmers/12day.js
+++ Programmers/12day.js
@@ -1,33 +1,23 @@
-<table>
-  <caption>학생별 과목 평균점수</caption>
-
-  <thead>
-    <tr>
-      <th scope="col">이름</th>
-      <th scope="col">과목</th>
-      <th scope="col">평균점수</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>김철수</td>
-      <td>영어</td>
-      <td>80점</td>
-    </tr>
-    <tr>
-      <td>김세미</td>
-      <td>수학</td>
-      <td>60점</td>
-    </tr>
-    <tr>
-      <td>김티스토리</td>
-      <td>체육</td>
-      <td>40점</td>
-    </tr>
-    <tr>
-      <td>철수세미</td>
-      <td>음악</td>
-      <td>100점</td>
-    </tr>
-  </tbody>
-</table>;
+function solution(my_string) {
+  let answer = "";
+  for (let i = 0; i < my_string.length; i++) {
+    const item = my_string[i];
+    if (item === "a") {
+      continue;
+    }
+    if (item === "e") {
+      continue;
+    }
+    if (item === "i") {
+      continue;
+    }
+    if (item === "o") {
+      continue;
+    }
+    if (item === "u") {
+      continue;
+    }
+    answer += item;
+  }
+  return answer;
+}
