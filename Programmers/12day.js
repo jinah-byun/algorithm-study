37309712bfca69cde579f02163eba71f78ce5bfb
jinah-byun <wlsdk120@gmail.com>
2023-05-04 21:36:58 +0900

    메모

--- Programmers/12day.js
+++ Programmers/12day.js
@@ -19,5 +19,30 @@ function solution(my_string) {
     }
     answer += item;
   }
+  function solution(my_string) {
+    let answer = "";
+    for (let i = 0; i < my_string.length; i++) {
+      const item = my_string[i];
+      if (item === "a") {
+        continue;
+      }
+      if (item === "e") {
+        continue;
+      }
+      if (item === "i") {
+        continue;
+      }
+      if (item === "o") {
+        continue;
+      }
+      if (item === "u") {
+        continue;
+      }
+      answer += item;
+    }
+    return answer;
+  }
   return answer;
 }
+
+// 2. 리팩토링
