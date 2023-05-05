280f08bbaf17c39ef6c8a1d6f542f142bb991ffc
jinah-byun <wlsdk120@gmail.com>
2023-05-05 20:16:58 +0900

    refactor: pg/문자열 정렬하기/H/1문제

--- Programmers/12day.js
+++ Programmers/12day.js
@@ -1,3 +1,4 @@
+//1. 모음제거
 function solution(my_string) {
   let answer = "";
   for (let i = 0; i < my_string.length; i++) {
@@ -19,30 +20,23 @@ function solution(my_string) {
     }
     answer += item;
   }
-  function solution(my_string) {
-    let answer = "";
-    for (let i = 0; i < my_string.length; i++) {
-      const item = my_string[i];
-      if (item === "a") {
-        continue;
-      }
-      if (item === "e") {
-        continue;
-      }
-      if (item === "i") {
-        continue;
-      }
-      if (item === "o") {
-        continue;
-      }
-      if (item === "u") {
-        continue;
-      }
-      answer += item;
-    }
-    return answer;
-  }
-  return answer;
 }
+// 1-1. 모음제거 리팩토링
+function solution(my_string) {
+  let answer = "";
+  const vowels = ["a", "e", "i", "o", "u"];
 
-// 2. 리팩토링
+  for (let i = 0; i < my_string.length; i++) {
+    const item = my_string[i];
+
+    let isVowel = false;
+    for (let j = 0; j < vowels.length; j++) {
+      if (item === vowels[j]) {
+        isVowel = true;
+        break;
+      }
+    }
+    if (isVowel) continue;
+    answer += item;
+  }
+}
