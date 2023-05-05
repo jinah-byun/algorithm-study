968759514782d8b2ed0eda68abb901f301ac1bf1
jinah-byun <wlsdk120@gmail.com>
2023-05-05 21:00:19 +0900

    unsolved: pg/문자열정렬하기(1)/H/1문제

--- Programmers/12day.js
+++ Programmers/12day.js
@@ -1,42 +1,56 @@
-//1. 모음제거
-function solution(my_string) {
-  let answer = "";
-  for (let i = 0; i < my_string.length; i++) {
-    const item = my_string[i];
-    if (item === "a") {
-      continue;
-    }
-    if (item === "e") {
-      continue;
-    }
-    if (item === "i") {
-      continue;
-    }
-    if (item === "o") {
-      continue;
-    }
-    if (item === "u") {
-      continue;
-    }
-    answer += item;
-  }
-}
-// 1-1. 모음제거 리팩토링
-function solution(my_string) {
-  let answer = "";
-  const vowels = ["a", "e", "i", "o", "u"];
+// //1. 모음제거
+// function solution(my_string) {
+//   let answer = "";
+//   for (let i = 0; i < my_string.length; i++) {
+//     const item = my_string[i];
+//     if (item === "a") {
+//       continue;
+//     }
+//     if (item === "e") {
+//       continue;
+//     }
+//     if (item === "i") {
+//       continue;
+//     }
+//     if (item === "o") {
+//       continue;
+//     }
+//     if (item === "u") {
+//       continue;
+//     }
+//     answer += item;
+//   }
+// }
+// // 1-1. 모음제거 리팩토링
+// function solution(my_string) {
+//   let answer = "";
+//   const vowels = ["a", "e", "i", "o", "u"];
 
-  for (let i = 0; i < my_string.length; i++) {
-    const item = my_string[i];
+//   for (let i = 0; i < my_string.length; i++) {
+//     const item = my_string[i];
 
-    let isVowel = false;
-    for (let j = 0; j < vowels.length; j++) {
-      if (item === vowels[j]) {
-        isVowel = true;
-        break;
-      }
+//     let isVowel = false;
+//     for (let j = 0; j < vowels.length; j++) {
+//       if (item === vowels[j]) {
+//         isVowel = true;
+//         break;
+//       }
+//     }
+//     if (isVowel) continue;
+//     answer += item;
+//   }
+// }
+
+//2. 문자열 정렬하기(1)-바스키코드
+function solution(my_string) {
+  var answer = [];
+  for (let i = 0; i < my_string.length; i++) {
+    const item = Number(my_string[i]); //문자를 하나씩 꺼낸다
+    if (Number.isNaN(item)) {
+      continue;
     }
-    if (isVowel) continue;
-    answer += item;
+    answer.push(item);
   }
+  answer.sort((a, b) => a - b);
+  return answer;
 }
